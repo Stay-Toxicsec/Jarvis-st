cb8b253f3896062834bb1857e3970013a184ddc4
Toxic Security <134473785+Stay-Toxicsec@users.noreply.github.com>
2023-05-24 11:04:11 +0530

    bcx.py

--- bcx.py
+++ bcx.py
@@ -1,3 +1,4 @@
+#this is just a refrence file, it has no use in main.py. but you can still run it individually
 
 import os
 import openai
@@ -5,7 +6,7 @@ from myapi import apikey
 
 openai.api_key = apikey
 
-a= input("enter command")
+a= input("enter command:  _")
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": a}])
 print(completion.choices[0].message.content)
